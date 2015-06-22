05d1fe19000f6ca65222fef6f3628d549c3b76a1
Oleksandr Buravlyov <oleksandr.buravlyov@globallogic.com>
2015-06-23 00:19:26 +0300

    add test1

--- testServerRunIC/ViewController.m
+++ testServerRunIC/ViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    self.label.text = @"master";
+    self.label.text = @"test1";
 }
 
 - (void)didReceiveMemoryWarning {
